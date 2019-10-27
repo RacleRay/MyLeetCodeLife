a6c33bc73a868843b86e68950cf5e98f23cd79b1
Racle <43697210+RacleRay@users.noreply.github.com>
2019-10-27 21:50:41 +0800

    add non_shared_substring

--- DataStructure&Algorithms_Coursera/StringAlgo/TrieAndSuffix/non_shared_substring/non_shared_substring.py
+++ DataStructure&Algorithms_Coursera/StringAlgo/TrieAndSuffix/non_shared_substring/non_shared_substring.py
@@ -1,9 +1,18 @@
 # python3
 import sys
 
-def solve (p, q):
-	result = p
-	return result
+
+def solve(p, q):
+	# naive
+    for i in range(min(len(p), len(q))):
+        for j in range(len(p) - i):
+            s = p[j: (j + i + 1)]
+            if not s in q:
+                return s
+
+    return
+
+
 
 p = sys.stdin.readline ().strip ()
 q = sys.stdin.readline ().strip ()
