e0a7804fd2bbc75d5ff3fb3357f5e4270f6197e7
Racle <43697210+RacleRay@users.noreply.github.com>
2020-01-04 00:04:07 +0800

    add 5

--- Book_part/Python_problems/Stack_Queue_Hash/5-栈模拟队列.py
+++ Book_part/Python_problems/Stack_Queue_Hash/5-栈模拟队列.py
@@ -11,4 +11,20 @@ class StackQueue:
         self.input_stack.push(item)
 
     def pop(self):
-        if self.output_stack
+        if self.output_stack.is_empty():
+            while not self.input_stack.is_empty():
+                self.output_stack.push(self.input_stack.pop())
+
+        return self.output_stack.pop()
+
+
+if __name__ == "__main__":
+    queue = StackQueue()
+
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
