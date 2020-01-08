2bd66db62382accd81a1e38ead00355c6a9573f2
Racle <43697210+RacleRay@users.noreply.github.com>
2020-01-09 00:05:35 +0800

    add tree1

--- Book_part/Python_problems/Tree/1-将有序数组放到二叉树中.py
+++ Book_part/Python_problems/Tree/1-将有序数组放到二叉树中.py
@@ -16,3 +16,19 @@ def array_to_tree(array):
     return root
 
 
+def mid_order_retrieve(root):
+    """中序遍历"""
+    if root is None:
+        return
+
+    mid_order_retrieve(root.leftChild)
+    print(root.data, end=' ')
+    mid_order_retrieve(root.rightChild)
+
+
+if __name__ == '__main__':
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    print(array)
+
+    root = array_to_tree(array)
+    mid_order_retrieve(root)
