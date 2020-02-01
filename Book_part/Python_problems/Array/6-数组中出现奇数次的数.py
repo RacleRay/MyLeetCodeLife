07d243bd6cbf26787bdacc1690b563098f8b2e8e
Racle <43697210+RacleRay@users.noreply.github.com>
2020-02-02 00:10:54 +0800

    add 6

--- Book_part/Python_problems/Array/6-数组中出现奇数次的数.py
+++ Book_part/Python_problems/Array/6-数组中出现奇数次的数.py
@@ -17,6 +17,35 @@ def get_single(arr):
             print('Single: ', k)
 
 
+def get_single_xor(arr):
+    if arr is None:
+        return None
+
+    result = 0
+    position = 0
+
+    for item in arr:
+        result = result ^ item
+
+    # 找到异或结果中为1的位置(第一个位置)
+    tmp = result
+    while tmp & 1 == 0:
+        position += 1
+        tmp >>= 1
+
+    # 将所有position位置为1的数与result异或（两个single值之一）
+    res_1 = 0
+    for item in arr:
+        if (item >> position) & 1 == 1:
+            res_1 = res_1 ^ item
+
+    res_2 = result ^ res_1  # 另一个single的值
+
+    print(res_1, res_2)
+
+
 if __name__ == "__main__":
     arr = array('B', [3, 5, 6, 6, 5, 7, 2, 2])
     get_single(arr)
+
+    get_single_xor(arr)
