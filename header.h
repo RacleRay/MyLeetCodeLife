413f5580a679686306c514f3859fe6f7e7c12c9e
Racle <43697210+RacleRay@users.noreply.github.com>
2023-12-03 18:51:26 +0800

    header

---
+++ header.h
@@ -0,0 +1,69 @@
+/*************************************************************************
+    > File Name: header.cpp
+    > Author: racle
+    > Mail: racleray@qq.com
+    > Created Time: 
+ ************************************************************************/
+
+#ifndef COMMON_LIBRARIES_H
+#define COMMON_LIBRARIES_H
+
+// IO流库
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+// 容器库
+#include <vector>
+#include <list>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+// 字符串库
+#include <string>
+#include <cstring>
+
+// 算法库
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+// 数学库
+#include <cmath>
+#include <random>
+
+// 时间库
+#include <chrono>
+#include <ctime>
+
+// 异常处理库
+#include <exception>
+#include <stdexcept>
+
+// 多线程库
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+// 文件系统库（C++17）
+//#include <filesystem>
+
+// 正则表达式库
+#include <regex>
+
+// JSON库
+//#include <json/json.h>
+
+// 图形库
+//#include <SFML/Graphics.hpp>
+
+// 类型
+#include <climits>
+
+
+using namespace std;
+
+#endif  // COMMON_LIBRARIES_H
---
+++ test.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*#pragma pack(1)*/
+typedef struct {
+    short a1;
+    char b1;
+    double c1;
+} AA;
+/*#pragma pack()*/
+
+
+typedef struct {
+    unsigned char x1:1;  // low address
+    unsigned char x2:3;
+    unsigned char x3:4;  // high address
+    unsigned char x5:4;  // high address
+    unsigned char x4:8;
+} BitField;
+
+
+union uu {
+    int a;
+    char b;
+};
+
+
+int main() {
+    int cc = 10;
+
+    union uu u;
+    u.a = 1;
+
+    int aaa = 10;
+
+    AA a;
+
+    printf("%lu\n", sizeof(a));
+    a.a1 = 1;
+
+
+    BitField bf;
+    bf.x1 = 0;
+    bf.x2 = 3;
+    bf.x3 = 0xc;
+
+    printf("bf = %hhx\n", bf);
+    printf("bf size = %lu\n", sizeof(bf));
+
+
+    srand(48);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\t", rand());
+    }
+    printf("\n");
+
+    return 0;
+}
