1087c24eeeef796f5e25247d2a2913b7e2501fb5
racleray <racleme@outlook.com>
2024-03-17 21:19:13 +0800

    compare different memory pool

--- Memory_Pool/main.cpp
+++ Memory_Pool/main.cpp
@@ -7,19 +7,86 @@
 #include "MemoryPool.h"
 #include "StackAlloc.hpp"
 #include "SGIPool.hpp"
+#include "NginxPool.hpp"
 
 
 #define ELEMS 1000000
 #define REPS 50
 
+
+//=================================================================================
+// test Nginx Memory Pool
+typedef struct Data Data_t;
+
+struct Data
+{
+    char* ptr;
+    FILE* pfile;
+};
+
+void clear_char(void* p1)
+{
+    char* p = (char*)p1;
+    printf("free ptr memory!\n");
+    free(p);
+}
+
+void clear_file(void* pf1)
+{
+    FILE* pf = (FILE*)pf1;
+    printf("close file!\n");
+    fclose(pf);
+}
+
+int test_nginx_pool()
+{
+    // NgxMemoryPool* pool = new NgxMemoryPool();
+    NgxMemoryPool pool;
+
+    void *pool_ptr = pool.ngx_create_pool(1024);
+    if (pool_ptr == nullptr) {
+        printf("ngx_create_pool failed!\n");
+        return -1;
+    }
+
+    // 小块内存
+    void *p1 = pool.ngx_palloc(128);
+    if (p1 == nullptr) {
+        printf("ngx_palloc 128 failed!\n");
+        return -1;
+    }
+
+    // 大块内存
+    Data_t *p2 = (Data_t*)pool.ngx_palloc(1024 * 1024);
+    if (p2 == nullptr) {
+        printf("ngx_palloc 1024 * 1024 failed!\n");
+        return -1;
+    }
+
+    p2->ptr = (char*)malloc(512);
+    strcpy(p2->ptr, "hello world");
+    p2->pfile = fopen("data.txt", "w");
+
+    // 自定义内存清理
+    ngx_pool_cleanup_s* clr1 = pool.ngx_pool_cleanup_add(sizeof(char*));
+    clr1->handler = clear_char;
+    clr1->data = p2->ptr;
+
+    ngx_pool_cleanup_s* clr2 = pool.ngx_pool_cleanup_add(sizeof(FILE*));
+    clr2->handler = clear_file;
+    clr2->data = p2->pfile;
+
+    // 内存清理，释放大内存块，释放小块内存
+    pool.ngx_destroy_pool();
+
+    return 0;
+}
+
+
 int main() {
-
-  std::cout << "Hello, World!" << std::endl;
-
   clock_t start;
 
-  std::cout << "Copyright (c) 2013 Cosku Acay, http://www.coskuacay.com\n";
-  std::cout << "Provided to compare the default allocator to MemoryPool.\n\n";
+  std::cout << "Provided to compare the default allocator, MemoryPool, SGI memory pool.\n\n";
 
   /* Use the default allocator */
   StackAlloc< int, std::allocator<int> > stackDefault;
@@ -46,8 +113,6 @@ int main() {
 
   /* Use MemoryPool */
   StackAlloc< int, MPL::MemoryPool<int> > stackPool;
-  // SGIallocator : 效果基本就是和 std::allocator 一样
-  // StackAlloc< int, SGIallocator<int> > stackPool;
   start = clock();
   for (int j = 0; j < REPS; j++) {
     assert(stackPool.empty());
@@ -70,6 +135,36 @@ int main() {
   std::cout << "MemoryPool Allocator Time: ";
   std::cout << (((double)clock() - start) / CLOCKS_PER_SEC) << "\n\n";
 
+  // SGIallocator : 效果基本就是和 std::allocator 一样
+  StackAlloc< int, SGIallocator<int> > stackSGIPool;
+  start = clock();
+  for (int j = 0; j < REPS; j++) {
+    assert(stackSGIPool.empty());
+    for (int i = 0; i < ELEMS / 4; i++) {
+      // Unroll to time the actual code and not the loop
+      stackSGIPool.push(i);
+      stackSGIPool.push(i);
+      stackSGIPool.push(i);
+      stackSGIPool.push(i);
+    }
+    // std::cout << ">>> [max_size] test :" << stackSGIPool.get_pool_size() << "\n\n";
+    for (int i = 0; i < ELEMS / 4; i++) {
+      // Unroll to time the actual code and not the loop
+      stackSGIPool.pop();
+      stackSGIPool.pop();
+      stackSGIPool.pop();
+      stackSGIPool.pop();
+    }
+  }
+  std::cout << "SGI Memory Pool Allocator Time: ";
+  std::cout << (((double)clock() - start) / CLOCKS_PER_SEC) << "\n\n";
+
+  std::cout << "Run NGINX Memory Pool test: \n";
+  int ret = test_nginx_pool();
+  if (ret < 0) { std::cout << "NGINX Memory Pool test failed.\n"; }
+  std::cout << "NGINX Memory Pool test passed.\n\n";
+
+  std::cout << "========================================================\n";
   std::cout << "Here is a secret: the best way of implementing a stack"
                " is a dynamic array.\n";
 
@@ -97,6 +192,7 @@ int main() {
   std::cout << (((double)clock() - start) / CLOCKS_PER_SEC) << "\n\n";
 
   std::cout << "The vector implementation will probably be faster.\n\n";
+  std::cout << "========================================================\n";
   std::cout
       << "MemoryPool still has a lot of uses though. Any type of tree"
          " and when you have multiple linked lists are some examples (they"
